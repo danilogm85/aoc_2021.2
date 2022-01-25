2eb84fcd613d6bb77a3616b8ec63ef4de3617436
danilogm85 <73174404+danilogm85@users.noreply.github.com>
2022-01-25 11:20:56 -0300

    Update ALU_Decoder.vhd

--- ALU_Decoder.vhd
+++ ALU_Decoder.vhd
@@ -1,12 +1,17 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
+--Componente que decodifica o codigo funct para definir os sinais de controle da ALU em instruções do tipo-R ou o dado ALUOp proveniente do main decoder
+--	para utilização da ALU em instruções que não possuem o funct.
+
 entity ALU_Decoder is
 	port(
 			Funct	: in std_logic_vector(5 downto 0);
 			ALUOp	: in std_logic_vector(1 downto 0);
 			
-			ALU_JR	: out std_logic;
+			--intruções jr e xor são do tipo r mas não utilizam a ALU. Como o campo Op delas são iguais, o main decoder nao consegue diferenciá-las
+			--	sendo necessário que o ALU decoder gere os bits ALU_JR e ALU_XOR para o main decoder conseguir realizar sua operação
+			ALU_JR	: out std_logic;	
 			ALU_NOR	: out std_logic;
 			ALU_XOR	: out std_logic;
 			ALUCtrl	: out std_logic_vector(2 downto 0)
@@ -18,7 +23,7 @@ begin
 
 	process(Funct,ALUOp) is
 	begin
-		case ALUOp is
+		case ALUOp is			--Case especificando as saidas para cada caso de ALUOp e Funct, de acordo com a tabela verdade
 			when "00" =>
 				ALU_JR	<= '0';
 				ALU_NOR	<= '0';
@@ -71,12 +76,12 @@ begin
 						
 					when "100111" =>
 						ALU_JR	<= '0';
-						ALU_NOR	<= '1';
+						ALU_NOR	<= '1';	--gera o sinal ALU_NOR para o main decoder configurar o datapath para realizar a inversao da saida da ALU
 						ALU_XOR	<= '0';
-						ALUCtrl	<=	"001"; --OR
+						ALUCtrl	<=	"001"; --OR	--manda a ALU realizar uma OR
 						
 					when "001000" =>
-						ALU_JR	<= '1';
+						ALU_JR	<= '1';		--para instrução jr gera o bit ALU_JR para o main decoder
 						ALU_NOR	<= '0';
 						ALU_XOR	<= '0';
 						ALUCtrl	<=	"011"; --ALU NOT USED
@@ -84,7 +89,7 @@ begin
 					when "100110" =>
 						ALU_JR	<= '0';
 						ALU_NOR	<= '0';
-						ALU_XOR	<= '1';
+						ALU_XOR	<= '1';		--para instrução XOR gera o bit ALU_XOR para o main decoder
 						ALUCtrl	<=	"011"; --ALU NOT USED
 						
 					when others =>
